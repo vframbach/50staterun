acc4788b37022541515050b684193464b3061da1
vframbach <vframbach@gmail.com>
2016-03-17 15:56:28 -0700

    added content to marathon and advice pages, code for back to top link doesn't work

---
+++ js/marathons.js
@@ -0,0 +1,6 @@
+if ( ($(window).height() + 100) < $(document).height() ) {
+    $('#top-link-block').removeClass('hidden').affix({
+        // how far to scroll down before link "slides" into view
+        offset: {top:100}
+    });
+}
